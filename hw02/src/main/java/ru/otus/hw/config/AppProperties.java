d41148a3261ae3736c47bbacd29b49bda4df0911
Artem Alimov <alimovae@bcs-bank.com>
2024-06-21 08:55:50 +0700

    исправление замечаний от 21.06.2024

--- hw02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -14,7 +14,8 @@ public class AppProperties implements TestConfig, TestFileNameProvider {
     // внедрить свойство из application.properties
     private String testFileName;
 
-    public AppProperties(@Value("${test.rightAnswersCountToPass:3}") int rightAnswersCountToPass, @Value("${test.fileName:questions.csv}") String testFileName) {
+    public AppProperties(@Value("${test.rightAnswersCountToPass:3}") int rightAnswersCountToPass,
+                         @Value("${test.fileName:questions.csv}") String testFileName) {
         this.rightAnswersCountToPass = rightAnswersCountToPass;
         this.testFileName = testFileName;
     }
--- hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -1,31 +1,32 @@
 package ru.otus.hw.dao;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CsvQuestionDaoTest {
 
-    private final QuestionDao questionDao = new CsvQuestionDao(new AppProperties(1, "questions-test.csv"));
+    private QuestionDao questionDao;
+
+    @BeforeEach
+    void setUp() {
+        var properties = new AppProperties(1, "questions-test.csv");
+        questionDao= new CsvQuestionDao(properties);
+    }
 
     @Test
     void findAll() {
-        var expectedQuestion = "Java is a programming language?";
+        var expectedQuestion = new Question("Java is a programming language?",
+                List.of(new Answer("yes", true),
+                        new Answer("no", false)));
         var questions = questionDao.findAll();
 
-        assertAll(
-                () -> assertThat(questions).isNotEmpty(),
-                () -> assertThat(questions.size()).isOne(),
-                () -> assertThat(questions.get(0).text()).isEqualTo(expectedQuestion),
-                () -> assertThat(questions.get(0).answers()).isNotEmpty(),
-                () -> assertThat(questions.get(0).answers().size()).isEqualTo(2),
-                () -> assertTrue(questions.get(0).answers().containsAll(List.of(new Answer("yes", true), new Answer("no", false))))
-        );
+        assertThat(questions).containsExactlyInAnyOrder(expectedQuestion);
     }
 }
